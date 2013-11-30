cb8492e30634b81b64149a334c68063f29b0d557
mjbondra <mjbondra@umich.edu>
2013-11-29 22:10:43 -0500

    add asynchronous loading of js resources; keep it attached to index-async.html for now

--- public/js/app-loader.js
+++ public/js/app-loader.js
@@ -1,11 +1,37 @@
-require([
-  '../lib/angular/angular.min', 
-  '../lib/angular-route/angular-route.min', 
-  '../lib/angular-resource/angular-resource.min',
+require.config({
+  paths: {
+    angular: '../lib/angular/angular.min',
+    angularRoute: '../lib/angular-route/angular-route.min',
+    angularResource: '../lib/angular-resource/angular-resource.min',
+    typeKit: '//use.typekit.net/uby8kic'
+  },
+  shim: {
+    angular: {
+      exports: 'angular' 
+    },
+    angularRoute: ['angular'],
+    angularResource: ['angular'],
+    app: ['angular'],
+    controllers: ['angular'],
+    services: ['angular']
+  }
+});
+
+/** do not bootstrap with AngularJS until we have asynchronously loaded all of the AngularJS-related scripts */
+window.name = 'NG_DEFER_BOOTSTRAP!';
+
+require([  
+  'angular',
+  'angularRoute',
+  'angularResource',
   'app',
   'controllers',
-  'services',
-  '//use.typekit.net/uby8kic.js'
-], function () {
-  try{Typekit.load();}catch(e){}
+  'services'
+], function (angular, angularRoute, angularResource, app, controllers, services) {
+  'use strict';
+  angular.resumeBootstrap();
+});
+
+require(['typeKit'], function (typeKit) {
+  Typekit.load();
 });
