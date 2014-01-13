1a5cdc1c8fde7cf88e6fb41196e31274d4d25288
mjbondra <mjbondra@gmail.com>
2014-01-13 18:57:36 -0500

    add gulp, remove grunt

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+  , gulp = require('gulp')
+  , concat = require('gulp-concat')
+  , rename = require('gulp-rename')
+  , uglify = require('gulp-uglify');
+
+gulp.task('requirejs', function () {
+  gulp.src('./client/assets/lib/requirejs/require.js')
+    .pipe(rename('require.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./client/assets/lib/requirejs/'));
+});
+
+gulp.task('config', function () {
+  if (!fs.existsSync('./server/config/config.js')) {
+    gulp.src('./server/config/config.default.js')
+      .pipe(rename('config.js'))
+      .pipe(gulp.dest('./server/config/'));
+  }
+});
+
+gulp.task('default', function () {
+  gulp.run('requirejs');
+});
