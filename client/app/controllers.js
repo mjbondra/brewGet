c4e8e2784d6a63f4ffdd19cef32a186ffd5de539
mjbondra <mjbondra@gmail.com>
2014-01-26 23:33:14 -0500

    change formData name of files

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -128,16 +128,16 @@ app.controller('AccountSettings', ['$scope', '$upload', 'Head', 'User', 'Auth', 
           url: 'api/users/' + $scope.username + '/images', //upload.php script, node.js route, or servlet url
           // method: POST or PUT,
           // headers: {'headerKey': 'headerValue'}, withCredential: true,
-          data: {myObj: $scope.myModelObj},
+          data: { myObj: $scope.myModelObj },
           file: file,
           // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
           /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-          //fileFormDataName: myFile,
+          fileFormDataName: 'image',
           /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
           //formDataAppender: function(formData, key, val){} 
-        }).progress(function(evt) {
+        }).progress(function (evt) {
           console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-        }).success(function(data, status, headers, config) {
+        }).success(function (data, status, headers, config) {
           // file is uploaded successfully
           console.log(data);
         });
