417aeb776e87f8f9b7aab3174ae752db667a3d1f
mjbondra <mjbondra@gmail.com>
2014-02-18 21:47:42 -0500

    update back-end controllers/routes

---
+++ server/app/controllers/beers.js
@@ -0,0 +1,25 @@
+
+/**
+ * Module dependencies
+ */
+var coBody = require('co-body')
+  , cU = require('../../assets/lib/common-utilities')
+  , mongoose = require('mongoose')
+  , msg = require('../../config/messages')
+  , Promise = require('bluebird')
+  , _ = require('underscore');
+
+/**
+ * Models
+ */
+var Image = mongoose.model('Image')
+  , Beer = mongoose.model('Beer');
+
+/**
+ * Mongo projection paramater; includes or excludes fields
+ */
+var projection = { _id: 0, __v: 0 };
+
+module.exports = {
+  index: function *(next) {}
+}
--- server/config/routes.js
+++ server/config/routes.js
@@ -7,7 +7,9 @@ var requires = require('../app/middleware/auth').requires;
 /**
  * Controllers
  */
-var locations = require('../app/controllers/locations')
+var beers = require('../app/controllers/beers')
+  , breweries = require('../app/controllers/breweries')
+  , locations = require('../app/controllers/locations')
   , navigation = require('../app/controllers/navigation')
   , posts = require('../app/controllers/posts')
   , users = require('../app/controllers/users');
@@ -17,21 +19,32 @@ module.exports = function (app) {
   // temporary test route
   app.get('/test-api/test.json', function *() {
     this.body = {
-      "name": "brewGet",
-      "description": "a node/koa app that facilitates the trading of beer"
+      name: 'brewGet',
+      description: 'a node/koa app that facilitates the trading of beer'
     }
   })
+
+  // beers
+  app.get('/api/beers', beers.index);
+
+  // breweries
+  app.get('/api/breweries', breweries.index);
+
+  // locations
+  app.get('/api/locations', locations.index);
+  app.get('/api/locations/:state', locations.state);
+  app.get('/api/locations/:state/:city', locations.city);
 
   // navigation
   app.get('/api/nav', navigation);
 
-  // users sessions
-  app.post('/api/users/sign-in', users.sessions.create);
-  app.delete('/api/users/sign-out', users.sessions.destroy);
-
   // posts
   app.get('/api/posts', posts.index);
   app.post('/api/posts', posts.create);
+
+  // users sessions
+  app.post('/api/users/sign-in', users.sessions.create);
+  app.delete('/api/users/sign-out', users.sessions.destroy);
 
   // users
   app.get('/api/users', users.index);
@@ -41,11 +54,6 @@ module.exports = function (app) {
   app.delete('/api/users/:slug', requires.self, users.destroy);
   app.post('/api/users/:slug/images', requires.self, users.images.create);
   app.delete('/api/users/:slug/images', requires.self, users.images.destroy);
-
-  // locations
-  app.get('/api/locations', locations.index);
-  app.get('/api/locations/:state', locations.state);
-  app.get('/api/locations/:state/:city', locations.city);
 
   // redirect all remaining GET method routes to angular router
   app.get('*', function* () {
