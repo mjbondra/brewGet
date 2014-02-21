6149eb29b8edb73e5ee0aee49e37c6dcce5cb642
mjbondra <mjbondra@gmail.com>
2014-02-20 23:46:57 -0500

    update beer model

--- server/app/models/beer.js
+++ server/app/models/beer.js
@@ -2,7 +2,8 @@
 /**
  * Module dependencies
  */
-var mongoose = require('mongoose')
+var cU = require('../../assets/lib/common-utilities')
+  , mongoose = require('mongoose')
   , Promise = require('bluebird')
   , Schema = mongoose.Schema;
 
@@ -15,7 +16,25 @@ BeerSchema.index({ slug: 1, 'brewery.slug': 1 }, { unique: true });
  * Pre-validation hook; Sanitizers
  */
 BeerSchema.pre('validate', function (next) {
-  next();
+  if (this.brewery && this.brewery.name) {
+    this.brewery.slug = cU.slug(this.brewery.name);
+    Promise.promisify(Brewery.findOne, Brewery)({ slug: this.brewery.slug }).bind(this).then(function (brewery) {
+      if (!brewery) {
+        brewery = new Brewery(this._doc.brewery);
+        Promise.promisify(brewery.save, brewery)().bind(this).then(function (brewery) {
+          this.brewery = brewery[0];
+          next();
+        }).catch(function (err) {
+          next();
+        });
+      } else {
+        this.brewery = brewery;
+        next();
+      }
+    }).catch(function (err) {
+      next();
+    });
+  } else next();
 });
 
 /**
