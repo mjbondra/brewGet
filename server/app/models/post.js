56dccfd8b32211978f4f8fee8cccbebdcc99a29b
mjbondra <mjbondra@gmail.com>
2014-02-20 22:04:08 -0500

    update posts model

--- server/app/models/post.js
+++ server/app/models/post.js
@@ -8,18 +8,19 @@ var cU = require('../../assets/lib/common-utilities')
   , Promise = require('bluebird')
   , sanitize = require('../../assets/lib/sanitizer-extended')
   , Schema = mongoose.Schema
-  , validate = require('../../assets/lib/validator-extended');
+  , validate = require('../../assets/lib/validator-extended')
+  , _ = require('underscore');
 
 /**
  * Schema dependencies; subdocuments
  */
 var ImageSchema = mongoose.model('Image').schema
   , Beer = mongoose.model('Beer')
-  , BeerSchema = Beer.schema
+  , BeerSchema = require('../../config/schemas').beer
   , CommentSchema = mongoose.model('Comment').schema;
 
 BeerSchema.date = Date;
-BeerSchema.year = String;
+BeerSchema.year = Number;
 
 /**
  * Post schema
@@ -56,11 +57,37 @@ var PostSchema = new Schema({
  */
 PostSchema.pre('validate', function (next) {
   if (!this.category && this.isNew) this.category = null;
-  var i = this.beers.length;
+  var i = this.beers.length
+    , beers = []
+    , _beers = []
+    , newBeers = [];
   while (i--) {
-    console.log(this.beers[i]);
+    if (this.beers[i].name && this.beers[i].brewery && this.beers[i].brewery.name) {
+      this.beers[i].slug = cU.slug(this.beers[i].name);
+      this.beers[i].brewery.slug = cU.slug(this.beers[i].brewery.name);
+      beers.push(Promise.promisify(Beer.findOne, Beer)({ slug: this.beers[i].slug, 'brewery.slug': this.beers[i].brewery.slug }));
+      _beers.push(this.beers[i]);
+    } 
   }
-  next();
+  Promise.all(beers).bind(this).then(function (beers) {
+    i = beers.length;
+    while (i--) {
+      if (!beers[i]) {
+        beers[i] = new Beer(_beers[i]);
+        newBeers.push(Promise.promisify(beers[i].save, beers[i])());
+      }
+    }
+    this.beers = beers;
+    if (newBeers.length > 0) {
+      Promise.all(newBeers).then(function (beers) {
+        next();
+      });
+    } else {
+      next();
+    }
+  }).catch(function (err) {
+    next();
+  });  
 });
 
 /**
