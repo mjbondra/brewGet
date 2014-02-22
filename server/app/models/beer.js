e6766b06d6f76ba9fa4d0317271cd338d0f1a49a
mjbondra <mjbondra@gmail.com>
2014-02-21 22:31:03 -0500

    update beer model

--- server/app/models/beer.js
+++ server/app/models/beer.js
@@ -8,7 +8,8 @@ var cU = require('../../assets/lib/common-utilities')
   , Schema = mongoose.Schema;
 
 var BeerSchema = new Schema(require('../../config/schemas').beer)
-  , Brewery = mongoose.model('Brewery');
+  , Brewery = mongoose.model('Brewery')
+  , Style = mongoose.model('Style');
 
 BeerSchema.index({ slug: 1, 'brewery.slug': 1 }, { unique: true });
 
@@ -16,25 +17,35 @@ BeerSchema.index({ slug: 1, 'brewery.slug': 1 }, { unique: true });
  * Pre-validation hook; Sanitizers
  */
 BeerSchema.pre('validate', function (next) {
+  var nestedDocs = [];
   if (this.brewery && this.brewery.name) {
     this.brewery.slug = cU.slug(this.brewery.name);
-    Promise.promisify(Brewery.findOne, Brewery)({ slug: this.brewery.slug }).bind(this).then(function (brewery) {
-      if (!brewery) {
-        brewery = new Brewery(this._doc.brewery);
-        Promise.promisify(brewery.save, brewery)().bind(this).then(function (brewery) {
-          this.brewery = brewery[0];
-          next();
-        }).catch(function (err) {
-          next();
-        });
-      } else {
-        this.brewery = brewery;
-        next();
-      }
-    }).catch(function (err) {
-      next();
-    });
-  } else next();
+    nestedDocs.push(Promise.promisify(Brewery.findOne, Brewery)({ slug: this.brewery.slug }));
+  }
+  if (this.style && this.style.name) {
+    this.style.slug = cU.slug(this.style.name);
+    nestedDocs.push(Promise.promisify(Style.findOne, Style)({ slug: this.style.slug }));
+  } 
+  
+  // if (this.brewery && this.brewery.name) {
+  //   this.brewery.slug = cU.slug(this.brewery.name);
+  //   Promise.promisify(Brewery.findOne, Brewery)({ slug: this.brewery.slug }).bind(this).then(function (brewery) {
+  //     if (!brewery) {
+  //       brewery = new Brewery(this._doc.brewery);
+  //       Promise.promisify(brewery.save, brewery)().bind(this).then(function (brewery) {
+  //         this.brewery = brewery[0];
+  //         next();
+  //       }).catch(function (err) {
+  //         next();
+  //       });
+  //     } else {
+  //       this.brewery = brewery;
+  //       next();
+  //     }
+  //   }).catch(function (err) {
+  //     next();
+  //   });
+  // } else next();
 });
 
 /**
