bc64e6c8e3a9253ddb97bd520247ea8be6624617
mjbondra <mjbondra@gmail.com>
2014-02-25 19:51:19 -0500

    update "html" controller on front-end

--- client/app/controllers/html.js
+++ client/app/controllers/html.js
@@ -15,45 +15,58 @@ app.controller('html.head', ['$scope', 'Head', function ($scope, Head) {
  */
 app.controller('html.nav', ['$scope', '$location', 'API', function ($scope, $location, API) {
 
+  /**
+   * Navigation functions
+   */
+
+  /*-- controller functions --*/
+
   // load nav
   var loadNav = function () {
     API('api/nav').success(function (data) {
       $scope.Nav = data;
     });
   };
-  loadNav();
-
-  // reload nav on 'reloadNav' event
-  $scope.$on('reloadNav', function () {
-    loadNav();
-  });
-
   // collapse expanded trees
   var collapseTrees = function () {
     $scope.expandedTree = false;
   };
-  collapseTrees();
 
-  // collapse trees on route change success
-  $scope.$on('$routeChangeSuccess', function () {
-    collapseTrees();
-  });
+  /*-- $scope functions --*/
 
   // set tree to expand based on passed context
   $scope.expandTree = function (context) {
     if (context === $scope.expandedTree || typeof context === 'undefined') $scope.expandedTree = false;
     else $scope.expandedTree = context;
   }
-
   // function used to determine expanded tree
   $scope.isExpanded = function (context) {
     if (context === $scope.expandedTree) return true;
     else return false;
   }
-
   // function used to determine active path
   $scope.isActive = function (href) {
     if (href === $location.path()) return true;
     else return false;
   }
+
+  /**
+   * Navigation events
+   */
+
+  /*-- broadcast events --*/
+
+  // reload nav on 'reloadNav' event
+  $scope.$on('reloadNav', function () {
+    loadNav();
+  });
+  // collapse trees on route change success
+  $scope.$on('$routeChangeSuccess', function () {
+    collapseTrees();
+  });
+
+  /*-- onload events --*/
+
+  loadNav();
+  collapseTrees();
 }]);
