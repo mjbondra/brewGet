3854ca8962f928976f3b5bd5e5b7913be57e7db5
mjbondra <mjbondra@gmail.com>
2014-03-01 00:17:18 -0500

    update autocomplete functionality

--- client/app/controllers/posts.js
+++ client/app/controllers/posts.js
@@ -32,17 +32,45 @@ app.controller('posts.new', ['$scope', 'Head', 'Post', 'Autocomplete', function 
   Head.description('New Post');
   $scope.post = new Post();
   $scope.post.beers = [{}];
+  $scope.addBeer = function () {
+    $scope.post.beers.push({});
+  }
   $scope.Autocomplete = function (type, input, index) {
     if (input && input.length > 1) {
       Autocomplete(type, input).success(function (data) {
-        $scope.post[type][index].autocomplete = data;
+        switch (type) {
+          case 'beers':
+            $scope.post.beers[index].autocomplete = data;
+            break;
+          case 'breweries':
+            $scope.post.beers[index].brewery.autocomplete = data;
+            break;
+          case 'styles':
+            $scope.post.beers[index].style.autocomplete = data;
+            break;
+        }
       });
     } else {
-      $scope.post[type][index].autocomplete = [];
+      switch (type) {
+        case 'beers':
+          $scope.post.beers[index].autocomplete = [];
+          break;
+        case 'breweries':
+          $scope.post.beers[index].brewery.autocomplete = [];
+          break;
+        case 'styles':
+          $scope.post.beers[index].style.autocomplete = [];
+      }
     }
   }
-  $scope.addBeer = function () {
-    $scope.post.beers.push({});
+  $scope.applyBeer = function (parentIndex, index) {
+    $scope.post.beers[parentIndex] = $scope.post.beers[parentIndex].autocomplete[index];
+  }
+  $scope.applyBrewery = function (parentIndex, index) {
+    $scope.post.beers[parentIndex].brewery = $scope.post.beers[parentIndex].brewery.autocomplete[index];
+  }
+  $scope.applyStyle = function (parentIndex, index) {
+    $scope.post.beers[parentIndex].style = $scope.post.beers[parentIndex].style.autocomplete[index];
   }
 }]);
 
